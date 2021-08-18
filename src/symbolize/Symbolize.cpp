9debfb21d7b3432d057911c4f55328934828d87e
Samuel Judson <sam@sjudson.com>
2021-08-18 14:51:46 -0400

    Start query config file parser.

--- src/symbolize/Symbolize.cpp
+++ src/symbolize/Symbolize.cpp
@@ -11,7 +11,23 @@
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
+#include <optional>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <toml++/toml.h>
+
 using namespace llvm;
+
+
+struct Configuration {
+  bool allow;            // whether program variables are provided as allowlist or denylist
+  std::string QueryFile; // location of query IR
+
+  std::vector<std::string> env; // environmental variables
+  std::vector<std::string> ist; // (input) state variables
+  std::vector<std::string> prg; // program variables
+};
 
 
 void symbolize( Function &F, DomTreeNodeBase<BasicBlock> *Root ) {
@@ -60,23 +76,28 @@ void symbolize( Function &F, DomTreeNodeBase<BasicBlock> *Root ) {
 
 
 struct Symbolize : PassInfoMixin<Symbolize> {
-  SMDiagnostic Err;
-  std::unique_ptr<Module> Query = nullptr;
-
   // since we can't take cmdline args with the new pass manager
   // we will need to hardcode in the location of the query code
   //
   // this should not be a problem since it'll be produced by an
   // automated pipeline...
-  std::string QueryFile = "../query.ll";
+  std::string ConfigFile = "../query.config.toml";
+  Configuration Config;
+
+  SMDiagnostic Err;
+  std::unique_ptr<Module> Query;
 
   PreservedAnalyses run( Function &F, FunctionAnalysisManager &FAM );
+  void parseConfig();
   void loadQuery( LLVMContext &Ctx );
 };
 
 
 PreservedAnalyses Symbolize::run ( Function &F, FunctionAnalysisManager &FAM ) {
-  if ( !Symbolize::Query ) loadQuery( F.getContext() );
+  if ( !Symbolize::Query ) { // todo: make sure this actually only runs once
+    parseConfig();
+    loadQuery( F.getContext() );
+  }
 
   DominatorTree *DT = &FAM.getResult<DominatorTreeAnalysis>( F );
   symbolize( F, DT->getRootNode() );
@@ -85,7 +106,47 @@ PreservedAnalyses Symbolize::run ( Function &F, FunctionAnalysisManager &FAM ) {
 }
 
 
-void Symbolize::loadQuery( LLVMContext &Ctx ) { Symbolize::Query = parseIRFile( Symbolize::QueryFile, Symbolize::Err, Ctx ); }
+void Symbolize::parseConfig() {
+
+  toml::table tbl;
+
+  try { tbl = toml::parse_file( Symbolize::ConfigFile ); }
+  catch ( const toml::parse_error& err ) {
+    std::cerr << "Unable to parse configuration file:\n" << err << "\n";
+    exit( 1 );
+  }
+
+  std::optional<std::string> icf = tbl[ "headers" ][ "inputConstraintsFile" ].value<std::string>();
+  // todo: error if not present
+  Symbolize::Config.QueryFile = *icf;
+
+  toml::array *env, *ist, *prg;
+  env = tbl[ "environmental" ][ "vars" ].as_array();
+  if ( !env ) {} // todo: error
+  ist = tbl[ "state" ][ "vars" ].as_array();
+  if ( !ist ) {} // todo: error
+
+  toml::array *allow = tbl[ "program" ][ "allow" ].as_array();
+  toml::array *deny  = tbl[ "program" ][ "deny" ].as_array();
+
+  if (  allow &&  deny ) {} // todo: error
+  if ( !allow && !deny ) {} // todo: error
+  Symbolize::Config.allow = ( allow ) ? ( ( prg = allow ) && true ) : ( ( prg = deny ) && false );
+
+  // todo: handle bad types
+  for ( toml::node& elem : *env )
+    { elem.visit( [&]( auto&& el ) noexcept { if constexpr ( toml::is_string<decltype(el)> ) Symbolize::Config.env.push_back( el.get() ); } ); };
+  for ( toml::node& elem : *ist )
+    { elem.visit( [&]( auto&& el ) noexcept { if constexpr ( toml::is_string<decltype(el)> ) Symbolize::Config.ist.push_back( el.get() ); } ); };
+  for ( toml::node& elem : *prg )
+    { elem.visit( [&]( auto&& el ) noexcept { if constexpr ( toml::is_string<decltype(el)> ) Symbolize::Config.prg.push_back( el.get() ); } ); };
+
+  return;
+}
+
+
+void Symbolize::loadQuery( LLVMContext &Ctx ) { Symbolize::Query = parseIRFile( Symbolize::Config.QueryFile, Symbolize::Err, Ctx ); }
+
 
 
 /****************************
