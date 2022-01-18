b529fe95ed772f18c64de80a918eb848740ae3a6
Samuel Judson <sam@sjudson.com>
2022-01-18 12:32:28 -0500

    Finish exercise setup with correct queries and randomizers.

--- examples/car2/queries/car.py
+++ examples/car2/queries/car.py
@@ -1,33 +1,33 @@
 import soidlib
 
-for .car import declare, cancel
+cardinalPPs = { '0' : 'North', '1' : 'East', '2' : 'South', '3' : 'West' }
+cardinalIDs = { 'North' : 0, 'East' : 1, 'South' : 2, 'West' : 3 }
+
 
 def introduction():
-    return
+    return "\n\nWorking adaptively, so no intro...\n"
 
 
 def declare():
 
-    cardinals = { 0 : 'North', 1 : 'East', 2 : 'South', 3 : 'West' }
-
     E = {}
-    for i in range( 3 ):
-        for j in range( 3 ):
-            E[ f'occupied_{i}_{j}' ]    = soidlib.types.bool( f'occupied_{i}_{j}',    pp = None,      raw = f'locs{i}{j}' )
-            E[ f'oriented_{i}_{j}' ]    = soidlib.types.u32(  f'oriented_{i}_{j}',    pp = cardinals, raw = f'orients{i}{j}' )
-            E[ f'leftsignal_{i}_{j}' ]  = soidlib.types.bool( f'leftsignal_{i}_{j}',  pp = None,      raw = f'sigs{i}{j}0' )
-            E[ f'rightsignal_{i}_{j}' ] = soidlib.types.bool( f'rightsignal_{i}_{j}', pp = None,      raw = f'sigs{i}{j}1' )
+    for i in range( 4 ):
+        for j in range( 4 ):
+            E[ f'occupied_{i}_{j}' ]    = soidlib.types.bool_bv( f'occupied_{i}_{j}',    pp = None,      raw = f'locs{i}{j}' )
+            E[ f'oriented_{i}_{j}' ]    = soidlib.types.u32_bv(  f'oriented_{i}_{j}',    pp = cardinalPPs, raw = f'orients{i}{j}' )
+            E[ f'leftsignal_{i}_{j}' ]  = soidlib.types.bool_bv( f'leftsignal_{i}_{j}',  pp = None,      raw = f'sigs{i}{j}0' )
+            E[ f'rightsignal_{i}_{j}' ] = soidlib.types.bool_bv( f'rightsignal_{i}_{j}', pp = None,      raw = f'sigs{i}{j}1' )
 
-    S = { 'curr_direction' : soidlib.types.u32(  'curr_direction', pp = cardinals, raw = None ),
-          'from'           : soidlib.types.u32(  'from',           pp = cardinals, raw = None ),
-          'to'             : soidlib.types.u32(  'to',             pp = cardinals, raw = None ),
-          'row'            : soidlib.types.u32(  'row',            pp = None,      raw = None ),
-          'col'            : soidlib.types.u32(  'col',            pp = None,      raw = None ),
-          'needs_turn'     : soidlib.types.bool( 'needs_turn',     pp = None,      raw = None ),
-          'has_turned'     : soidlib.types.bool( 'has_turned',     pp = None,      raw = None ) }
+    S = { 'curr_direction' : soidlib.types.u32_bv(  'curr_direction', pp = cardinalPPs, raw = None ),
+          'from'           : soidlib.types.u32_bv(  'from',           pp = cardinalPPs, raw = None ),
+          'to'             : soidlib.types.u32_bv(  'to',             pp = cardinalPPs, raw = None ),
+          'row'            : soidlib.types.u32_bv(  'row',            pp = None,      raw = None ),
+          'col'            : soidlib.types.u32_bv(  'col',            pp = None,      raw = None ),
+          'needs_turn'     : soidlib.types.bool_bv( 'needs_turn',     pp = None,      raw = None ),
+          'has_turned'     : soidlib.types.bool_bv( 'has_turned',     pp = None,      raw = None ) }
 
-    P = { 'row'  : soidlib.types.u32(  'row',   pp = None, raw = '__soid__row' ),
-          'col'  : soidlib.types.u32(  'col',   pp = None, raw = '__soid__col' ),
-          'move' : soidlib.types.bool( 'move', pp = None, raw = '__soid__m' ) }
+    P = { 'new_row'  : soidlib.types.u32_bv(  'new_row',   pp = None, raw = None ),
+          'new_col'  : soidlib.types.u32_bv(  'new_col',   pp = None, raw = None ),
+          'move'     : soidlib.types.bool_bv( 'move',      pp = None, raw = 'm' ) }
 
     return E, S, P
