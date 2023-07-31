bc2ddae8f0b49f9e755ce1fcf7eb38716b403ffe
Samuel Judson <sam@sjudson.com>
2023-07-31 15:57:07 -0400

    Finish decision tree classifier.

--- examples/dt/src/classify.c
+++ examples/dt/src/classify.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <graphviz/cgraph.h>
 
 typedef struct Node {
@@ -28,8 +29,9 @@ int parse_label( Node *N, Agnode_t *n ) {
 
     tok = strtok( tok, " " );
     while( tok ) { ptok = tok; tok = strtok( NULL, " " );  } // iterate to end of test string
-    N->test = strtod( ptok, NULL );
+    N->test  = strtod( ptok, NULL );
 
+    N->class = -1;
     return 0;
   }
 
@@ -83,7 +85,6 @@ void make_tree( Node *root ) {
 
   n = agfstnode( g );
 
-  root = ( Node* ) malloc( sizeof( Node ) );
   leaf = parse_label( root, n );
 
   if ( !leaf ) make_children( g, root, n );
@@ -93,10 +94,31 @@ void make_tree( Node *root ) {
 }
 
 
+int traverse( Node *N, double *fv ) {
+  if ( N->class >= 0 ) return N->class;
+  return ( fv[ N->tidx ] <= N->test ) ? traverse( N->tchild, fv ) : traverse( N->fchild, fv );
+}
+
+
+int classify( Node *N, double *data ) {
+  double bmi = data[ 6 ] / ( pow( data[ 5 ], 2 ) );
+  double fv[ 8 ] = { data[ 0 ], data[ 1 ], data[ 2 ], data[ 3 ], data[ 4 ], bmi, data[ 7 ], data[ 8 ] };
+
+  return traverse( N, fv );
+}
+
+
 int main( int argc, char **argv ) {
 
-  Node *root;
+  Node *root = ( Node* ) malloc( sizeof( Node ) );
   make_tree( root );
+
+  double data[ 9 ] = { 1.0, 199.0, 76.0, 43.0, 0.0, 54.0, 249.973, 1.394, 22.0 };
+
+  int cls = classify( root, data );
+  const char *clss[ 2 ] = { "FALSE", "TRUE" };
+
+  printf( "Test Instance classified to %s\n", clss[ cls ] );
 
   return 0;
 }
